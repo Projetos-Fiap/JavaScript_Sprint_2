e1ce29b5bbf3054e6d9b05ae5925f803c26db9e1
alevaiano <rm551497@fiap.com.br>
2023-05-21 16:08:55 -0300

    js

--- js/java.js
+++ js/java.js
@@ -1,53 +1,77 @@
 document.getElementById("registrationForm").addEventListener("submit", function(event) {
   event.preventDefault(); // Impede o envio do formulário padrão
 
-  // Lógica de validação dos campos do formulário
-  var name = document.getElementById("name").value;
-  var lastName = document.getElementById("lastName").value;
-  var cpfCnpj = document.getElementById("cpfCnpj").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  var confirmPassword = document.getElementById("confirmPassword").value;
-  var cep = document.getElementById("cep").value;
+  let errorMessages = [];
+  let isValid = false;
 
-  var errorMessages = [];
+  while (!isValid) {
+    // Lógica de validação dos campos do formulário
+    let name = document.getElementById("name").value;
+    let lastName = document.getElementById("lastName").value;
+    let cpf = document.getElementById("cpf").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+    let cep = document.getElementById("cep").value;
 
-  if (name.length < 5) {
-    errorMessages.push("O nome deve ter pelo menos 5 letras.");
+    errorMessages = [];
+
+    // Verificação dos campos do formulário
+    if (name.length < 5) {
+      errorMessages.push("O nome deve ter pelo menos 5 letras.");
+    }
+
+    if (lastName.length < 5) {
+      errorMessages.push("O sobrenome deve ter pelo menos 5 letras.");
+    }
+
+    if (!/^\d{11}$/.test(cpf)) {
+      errorMessages.push("O CPF deve conter 11 números.");
+    }
+
+    if (email.length < 5 || !email.includes("@")) {
+      errorMessages.push("O email deve conter pelo menos 5 letras e um '@'.");
+    }
+
+    if (!/^\d{8}$/.test(cep)) {
+      errorMessages.push("O CEP deve conter 8 números.");
+    }
+
+    if (password !== confirmPassword) {
+      errorMessages.push("As senhas não correspondem.");
+    }
+
+    // Verificação de erros
+    if (errorMessages.length > 0) {
+      // Exibir mensagens de erro
+      document.getElementById("msgError").innerHTML = errorMessages.join("<br>");
+      break; // Sai do loop se houver erros
+    } else {
+      isValid = true; // Define isValid como true para sair do loop
+    }
   }
 
-  if (lastName.length < 5) {
-    errorMessages.push("O sobrenome deve ter pelo menos 5 letras.");
-  }
+  if (isValid) {
+    // Limpar mensagens de erro
+    document.getElementById("msgError").innerHTML = "";
 
-  if (!/^\d{11}$|^\d{14}$/.test(cpfCnpj)) {
-    errorMessages.push("O CPF/CNPJ deve conter 11 ou 14 números.");
-  }
+    // Exibir mensagem de sucesso
+    document.getElementById("msgSuccess").innerHTML = "Cadastro realizado com sucesso!";
 
-  if (email.length < 5 || !email.includes("@")) {
-    errorMessages.push("O email deve conter pelo menos 5 letras e um '@'.");
-  }
+    // Reseta o formulário
+    document.getElementById("registrationForm").reset();
 
-  if (!/^\d{8}$/.test(cep)) {
-    errorMessages.push("O CEP deve conter 8 números.");
-  }
-
-  if (password !== confirmPassword) {
-    errorMessages.push("As senhas não correspondem.");
-  }
-
-  if (errorMessages.length > 0) {
-    document.getElementById("msgError").innerHTML = errorMessages.join("<br>");
-  } else {
-    document.getElementById("msgError").innerHTML = ""; // Limpa a mensagem de erro
-    document.getElementById("msgSuccess").innerHTML = "Cadastro realizado com sucesso!"; // Exibe mensagem de sucesso
-    document.getElementById("registrationForm").reset(); // Reseta o formulário
-    
     // Armazena os dados no localStorage
     localStorage.setItem('name', name);
     localStorage.setItem('lastName', lastName);
-    localStorage.setItem('cpfCnpj', cpfCnpj);
+    localStorage.setItem('cpf', cpf);
     localStorage.setItem('email', email);
     localStorage.setItem('cep', cep);
+
+    console.log("Cadastro concluído");
+
+    // Redireciona para a página principal
+    window.location.replace("../index.html");
   }
 });
+
